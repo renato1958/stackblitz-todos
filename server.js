3dbaacbdf80c90a3ab8b9386d8d2d55f118052f9
renato1958 <felisi.renato@gmail.com>
2023-03-11 21:55:12 +0100

    1 file modified, 1 added

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.use('/api/todos', todosRouter);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server in ascolto sulla porta ${port}`);
 });
