2e89bd81943a2d21e9fd7be60df965bb634d41cc
renato1958 <felisi.renato@gmail.com>
2023-03-11 13:56:44 +0100

    3 files modified, 2 deleted

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+import logger from 'morgan';
 
 const app = express();
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 3000;
 
